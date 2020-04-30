9b9657ab0f44fc5839536a928b50db24249186bc
Yoshifumi Sato <sato_yos@nifty.com>
2020-04-30 10:34:04 +0900

    Initial Commit

---
+++ WhatsNextScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsNextScreen
+//
+//  Created by Yoshifumi Sato on 2020/04/30.
+//  Copyright © 2020 Yoshifumi Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
