76545b1fd448afdf51e547d0d2547c872883680f
Yoshifumi Sato <sato_yos@nifty.com>
2020-04-30 21:50:10 +0900

    set navigationBarButtons

---
+++ WhatsNextScreen/WhatsNextScreenDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  WhatsNextScreenDelegate.swift
+//  WhatsNextScreen
+//
+//  Created by Yoshifumi Sato on 2020/04/30.
+//  Copyright © 2020 Yoshifumi Sato. All rights reserved.
+//
+
+import UIKit
+
+extension WhatsNextViewController {
+
+    @objc func gearButtonTapped() {
+        assert(true, "歯車ボタンが押された！")
+    }
+    
+    @objc func exitButtonTapped() {
+        assert(true, "Exitボタンが押された！")
+    }
+}
