82e6c237083223b703f095f8af2360aad66017b3
Yoshifumi Sato <sato_yos@nifty.com>
2020-04-30 15:17:59 +0900

    Add unit test for WhatsNextScreen

---
+++ WhatsNextScreenTests/WhatsNextScreenTest.swift
@@ -0,0 +1,34 @@
+//
+//  WhatsNextScreenTest.swift
+//  Shuffle100Tests
+//
+//  Created by Yoshifumi Sato on 2020/04/27.
+//  Copyright © 2020 里 佳史. All rights reserved.
+//
+
+import XCTest
+
+class WhatsNextScreenTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func test_initialScreen() {
+        // given
+        let screen = WhatsNextViewController()
+        // when
+        screen.loadViewIfNeeded()
+        screen.view.layoutSubviews()
+        // then
+        XCTAssertEqual(screen.title, "次はどうする？")
+        XCTAssertEqual(screen.view.subviews.count, 3)
+        XCTAssertEqual(screen.refrainButton.title(for: .normal), "下の句をもう一度読む")
+        XCTAssertEqual(screen.refrainButton.frame.size.width, screen.view.frame.size.width * 0.8)
+    }
+
+}
